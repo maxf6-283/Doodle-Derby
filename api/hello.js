8921e13f98966e92771e22e419db67040ddb466a
AllocatedArtist <senorsiete2006@gmail.com>
2026-01-13 21:54:28 -0800

    added vite and playroomkit + small demo

--- api/hello.js
+++ api/hello.js
@@ -1,3 +1,16 @@
-export default function handler(req, res) {
-  res.status(200).json({ message: "Hello from the server!" });
-}
+import { insertCoin, onPlayerJoin, getParticipants } from "playroomkit";
+
+await insertCoin({});
+
+let playerList = document.getElementById("playerList");
+
+onPlayerJoin(player => {
+  // This relies on the built in lobby
+  // system. Will have to change how this 
+  // works if/when we make our own custom
+  // lobby.
+  let name = player.getProfile().name;
+  let playerNode = document.createElement("li");
+  playerNode.textContent = name;
+  playerList.appendChild(playerNode);
+})
