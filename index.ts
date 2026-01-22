a6f536c6197ac9b629d48646d7b7cd0d00ffbead
maxf6-283 <maxf6.283@gmail.com>
2026-01-22 10:16:59 -0800

    Show code in lobby

--- index.ts
+++ index.ts
@@ -4,7 +4,7 @@ const join_button = document.getElementById("join-button") as HTMLButtonElement
 export function joinLobby() {
   const code = code_textbox.value
 
-  window.location.href = window.location.href.split("/")[0] + "/lobby.html#r=" + code
+  window.location.href = window.location.href.split("/")[0] + "/lobby.html#r=R" + code
 }
 
 join_button.addEventListener("click", joinLobby)
