7d12888c2f4a6a4c2d171dbbd29d9bd4da05ebe8
Neel Dharm <dharmneel21@gmail.com>
2026-01-24 20:31:47 -0800

    added brush size button controls and canvas page

--- draw_canvas_example.ts
+++ draw_canvas_example.ts
@@ -4,25 +4,44 @@ import { PaintCanvas } from "./api/draw/painting"
 
 let stage = new konva.Stage({
   container: 'container',
-  width: window.innerWidth,
-  height: window.innerHeight
+  width: 600,
+  height: 600
 });
 
 const canvas: HTMLCanvasElement = document.createElement('canvas');
 canvas.width = stage.width();
 canvas.height = stage.height();
 
+const small_button = document.getElementById('small-button') as HTMLInputElement;
+const med_button = document.getElementById('med-button') as HTMLInputElement;
+const large_button = document.getElementById('large-button') as HTMLInputElement;
+const color_button = document.getElementById('color-button') as HTMLInputElement;
+
 let paint = new PaintCanvas(
   canvas,
   { x: 0, y: 0 },
   stage,
-  { color: "#ff0000", strokeWidth: 10 }
+  { color: "#0000ff", strokeWidth: 10 }
 );
+
+export function changeBrushSize(size: number) {
+    paint.setBrushStrokeWidth(size);
+}
+
+export function changeColor(color: string) {
+    paint.setBrushColor(color);
+}
+
+small_button.addEventListener('click', () => changeBrushSize(10));
+med_button.addEventListener('click', () => changeBrushSize(20));
+large_button.addEventListener('click', () => changeBrushSize(30));
+// color_button.addEventListener('change', () => changeColor(color_button.value));
 
 window.addEventListener("keydown", ev => {
   if (ev.key == "e") {
     paint.setErasing(!paint.isErasing);
   }
+
   if (ev.key == "u") {
     paint.undo();
   }
