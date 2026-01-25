83e9d2169e34f585714502c92261d7a5ef8f3502
maxf6-283 <maxf6.283@gmail.com>
2026-01-24 21:03:59 -0800

    Game starting

--- lobby.ts
+++ lobby.ts
@@ -1,4 +1,4 @@
-import { insertCoin, onPlayerJoin, myPlayer, getRoomCode, getParticipants, isHost, RPC, PlayerState, getState, setState } from "playroomkit";
+import { insertCoin, onPlayerJoin, myPlayer, getRoomCode, getParticipants, isHost, PlayerState, getState, setState, onDisconnect, RPC } from "playroomkit";
 
 try {
   await insertCoin({
@@ -19,6 +19,9 @@ const ready_button = document.getElementById("ready-button") as HTMLButtonElemen
 const settings_button = document.getElementById("settings-button") as HTMLButtonElement
 const settings = document.getElementById("settings") as HTMLDivElement
 const setting_of_some_kind = document.getElementById("setting-of-some-kind") as HTMLInputElement
+const start_game = document.getElementById("start-game") as HTMLButtonElement
+const lobby = document.getElementById("lobby") as HTMLDivElement
+const game = document.getElementById("game") as HTMLDivElement
 
 settings_button.addEventListener("click", () => {
   settings.hidden = !settings.hidden
@@ -29,6 +32,7 @@ myPlayer().setState("ready", false, true);
 const my_id = myPlayer().id
 
 let player_nodes: { [p: string]: HTMLLIElement } = {}
+let players_ready: { [p: string]: boolean } = {}
 
 onPlayerJoin(player => {
   let name = player.getState("name");
@@ -48,6 +52,9 @@ function updatePlayerName(player: PlayerState) {
 
   if (player.getState("ready")) {
     player_nodes[player.id].textContent += " (ready)"
+    players_ready[player.id] = true
+  } else {
+    players_ready[player.id] = false
   }
   if (player.id == my_id) {
     const bold = document.createElement("strong")
@@ -63,6 +70,17 @@ function updatePlayerName(player: PlayerState) {
   }
 }
 
+function allReady(): boolean {
+  let all_ready = true;
+  for (const [_, ready] of Object.entries(players_ready)) {
+    if (!ready) {
+      all_ready = false
+      break
+    }
+  }
+  return all_ready
+}
+
 // check for changes
 setInterval(() => {
   let players = getParticipants()
@@ -71,13 +89,17 @@ setInterval(() => {
   }
   updateSettings()
 
-  code_span.innerText = getRoomCode() ?? "Error";
-  if (!getRoomCode()) {
-    // we have been kicked
-    alert("Kicked from room")
-    window.location.href = "/"
+  if (isHost()) {
+    start_game.disabled = !allReady()
   }
+
+  code_span.innerText = getRoomCode() ?? "Error";
 }, 250)
+
+onDisconnect((ev) => {
+  alert(`Kicked from room: ${ev.reason}`)
+  window.location.href = "/"
+})
 
 
 name_update_button.addEventListener("click", () => {
@@ -120,4 +142,15 @@ function updateSettings() {
 setting_of_some_kind.addEventListener("keydown", (ev) => {
   if (ev.key != "Enter") return
   updateSettings()
+})
+
+RPC.register("begin_game", async () => {
+  lobby.hidden = true
+  game.hidden = false
+})
+
+start_game.addEventListener("click", () => {
+  if (isHost() && allReady()) {
+    RPC.call("begin_game", {})
+  }
 })
