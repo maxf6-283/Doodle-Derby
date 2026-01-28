2f6d82dd1fa0e55d807ce15df5baf5da27caf54d
Zhiheng Zhou <zhz206@ucsd.edu>
2026-01-28 15:56:02 -0800

    Some additional ideas on changing brush size and color.

--- draw_canvas_example.ts
+++ draw_canvas_example.ts
@@ -12,10 +12,17 @@ const canvas: HTMLCanvasElement = document.createElement('canvas');
 canvas.width = stage.width();
 canvas.height = stage.height();
 
-const small_button = document.getElementById('small-button') as HTMLInputElement;
-const med_button = document.getElementById('med-button') as HTMLInputElement;
-const large_button = document.getElementById('large-button') as HTMLInputElement;
-const color_button = document.getElementById('color-button') as HTMLInputElement;
+//const small_button = document.getElementById('small-button') as HTMLInputElement;
+//const med_button = document.getElementById('med-button') as HTMLInputElement;
+//const large_button = document.getElementById('large-button') as HTMLInputElement;
+//const color_button = document.getElementById('color-button') as HTMLInputElement;
+const slider = document.getElementById('sliderContainer') as HTMLDivElement;
+const brush_size = document.getElementById('brushSize') as HTMLInputElement;
+const brush_value = document.getElementById('brushValue') as HTMLElement;
+const brush_btn = document.getElementById('brush') as HTMLButtonElement;
+const color_btn = document.getElementById('color_change') as HTMLButtonElement;
+const cl_menu = document.getElementById('cl_menu') as HTMLDivElement;
+const red_btn = document.getElementById('red') as HTMLButtonElement;
 
 let paint = new PaintCanvas(
   canvas,
@@ -32,10 +39,39 @@ export function changeColor(color: string) {
     paint.setBrushColor(color);
 }
 
-small_button.addEventListener('click', () => changeBrushSize(10));
-med_button.addEventListener('click', () => changeBrushSize(20));
-large_button.addEventListener('click', () => changeBrushSize(30));
+//small_button.addEventListener('click', () => changeBrushSize(10));
+//med_button.addEventListener('click', () => changeBrushSize(20));
+//large_button.addEventListener('click', () => changeBrushSize(30));
 // color_button.addEventListener('change', () => changeColor(color_button.value));
+
+color_btn.addEventListener('click', (ev) => {
+    ev.stopPropagation();
+    cl_menu.classList.toggle('hidden');
+})
+red_btn.addEventListener('click', () => {
+    changeColor('red');
+})
+brush_size.addEventListener('input', () => {
+    brush_value.innerText = brush_size.value;
+    changeBrushSize(Number(brush_size.value));
+})
+
+brush_btn.addEventListener('click', (ev) => {
+    ev.stopPropagation();
+    slider.classList.toggle('hidden');
+})
+slider.addEventListener('click', (ev) => {
+    ev.stopPropagation();
+});
+
+window.addEventListener('click', () => {
+    if (!slider.classList.contains('hidden')) {
+        slider.classList.add('hidden');
+    }
+    if (!cl_menu.classList.contains('hidden')) {
+        cl_menu.classList.add('hidden');
+    }
+});
 
 window.addEventListener("keydown", ev => {
   if (ev.key == "e") {
