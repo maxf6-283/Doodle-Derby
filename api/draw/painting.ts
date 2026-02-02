5a4cec40d21dca585ee168a0eeb50fb4791c23c7
AllocatedArtist <senorsiete2006@gmail.com>
2026-02-01 18:21:27 -0800

    added fill bucket and replaced canvas drawing

--- api/draw/painting.ts
+++ api/draw/painting.ts
@@ -1,5 +1,8 @@
 import Konva from "konva"
 
+import { CurveInterpolator } from "curve-interpolator";
+import { Vector2d } from "konva/lib/types";
+
 export interface Brush {
   // Hex (RGB) -- Ex: #FFFFFF
   color: string
@@ -8,15 +11,18 @@ export interface Brush {
 
 export enum PaintActionType {
   Draw,
-  Erase
+  Erase,
+  Fill
 }
 
+type PointsArray = [number, number][]
+
 // Array of tuples to construct a user's stroke
-type PointsArray = [Konva.Vector2d, Konva.Vector2d][];
+type Strokes = PointsArray[];
 
 export interface PaintAction {
   // Array of connecting points
-  points: PointsArray,
+  points: Strokes,
   type: PaintActionType,
   brush: Brush
 }
@@ -24,13 +30,26 @@ export interface PaintAction {
 export class PaintCanvas {
   private image: Konva.Image
   private layer: Konva.Layer
-  private context: CanvasRenderingContext2D | null
+  private context: CanvasRenderingContext2D
 
   private _isErasing: boolean
   private currentBrush: Brush
 
   private undoBuffer: PaintAction[]
   private redoBuffer: PaintAction[]
+
+  private canvasWidth: number
+  private canvasHeight: number
+
+  private pointsBuffer: [number, number][] = []
+
+  private requiredPoints: number = 4
+
+  // Track if fill bucket was used
+  private hasFilled: boolean = false;
+
+  // Track if undo or redo was used
+  private hasReverted: boolean = false;
 
   // Initializes internals of PaintCanvas and sets up
   // callbacks for drawing.
@@ -50,10 +69,7 @@ export class PaintCanvas {
 
     this.layer.add(this.image);
 
-    this.context = canvas.getContext('2d');
-
-    (this.context as CanvasRenderingContext2D).fillStyle = "#ffffff";
-    this.context?.fillRect(0, 0, this.image.getWidth(), this.image.getHeight());
+    this.context = canvas.getContext('2d') as CanvasRenderingContext2D;
 
     this.currentBrush = brush;
 
@@ -62,8 +78,6 @@ export class PaintCanvas {
     this.undoBuffer = [];
     this.redoBuffer = [];
 
-    let lastMousePos: Konva.Vector2d;
-    let isPainting = false;
 
     // default initialization, it
     // doesn't mean anything yet.
@@ -77,77 +91,101 @@ export class PaintCanvas {
       brush: { ...this.currentBrush }
     };
 
-    const updateEraserState = () => {
-      let context = this.context as CanvasRenderingContext2D;
-      if (this.isErasing) {
-        context.globalCompositeOperation = "destination-out";
-      } else {
-        context.globalCompositeOperation = "source-over";
-      }
-    }
+    let isPainting = false;
+    let imageData: ImageData | null = null;
 
-    this.image.on('mousedown', () => {
-      lastMousePos = stage.getPointerPosition() as Konva.Vector2d;
-      isPainting = true;
-      updateEraserState();
+    const submitDraw = () => {
+      if (imageData != null) {
+        this.context.putImageData(imageData, 0, 0);
+      }
+
+      this.layer.batchDraw();
+    };
+
+    const draw = (imageData: ImageData, clicked: boolean = false) => {
+      let currentMousePos = stage.pointerPos as Vector2d;
+      let pos: [number, number] = [
+        currentMousePos.x - this.layer.getPosition().x,
+        currentMousePos.y - this.layer.getPosition().y
+      ];
+
+      this.pointsBuffer.push(pos);
+
+      if (this.pointsBuffer.length > this.requiredPoints) {
+        submitDraw();
+        paintAction.points.push(this.pointsBuffer.slice());
+        this.pointsBuffer.shift();
+      }
+
+      if (clicked) {
+        paintAction.points.push(this.pointsBuffer.slice());
+      }
+
+      this.drawPoint(this.pointsBuffer, brush, this.isErasing, imageData);
+    };
+
+    const finish = () => {
+      submitDraw();
+      isPainting = false;
+      this.undoBuffer.push({
+        ...paintAction
+      });
 
       paintAction.points = [];
+      this.pointsBuffer = [];
+    }
+
+    this.image.on('mouseup', () => {
+      finish();
+    });
+
+    this.image.on('mouseleave', () => {
+      if (!isPainting) return;
+      if (imageData == null) return;
+      draw(imageData);
+      finish();
+    });
+
+    this.image.on('mousedown', () => {
+      if (imageData == null ||
+        this.hasFilled ||
+        this.hasReverted
+      ) {
+        imageData = this.getImageData();
+        this.hasFilled = false;
+        this.hasReverted = false;
+      }
+
+      isPainting = true;
+
       paintAction.brush = {
         ...this.currentBrush
       };
-    });
 
-    this.image.on('mouseup', () => {
-      isPainting = false;
-
-      this.drawPoint(
-        lastMousePos,
-        lastMousePos,
-        brush,
-        this.isErasing
-      );
-
-      paintAction.type =
-        this.isErasing ?
-          PaintActionType.Erase :
-          PaintActionType.Draw;
-
-      paintAction.points.push([
-        lastMousePos,
-        lastMousePos
-      ]);
+      paintAction.type = this.isErasing ?
+        PaintActionType.Erase :
+        PaintActionType.Draw;
 
       this.redoBuffer = [];
-      this.undoBuffer.push({ ...paintAction });
+
+      draw(imageData, true);
     });
 
-    this.layer.on('mousemove', () => {
-      if (!isPainting) return;
-
-      let localPos = {
-        x: lastMousePos.x - this.image.x(),
-        y: lastMousePos.y - this.image.y(),
-      };
-
-      let newMousePos = stage.getPointerPosition() as Konva.Vector2d;
-      let newLocalPos = {
-        x: newMousePos.x - this.image.x(),
-        y: newMousePos.y - this.image.y()
-      };
-
-      updateEraserState();
-
-      this.drawPoint(localPos, newLocalPos, brush, this.isErasing);
-
-      paintAction.points.push([
-        localPos,
-        newLocalPos
-      ]);
-
-      lastMousePos = newLocalPos;
+    this.image.on('mousemove', () => {
+      if (!isPainting || imageData == null) return;
+      draw(imageData);
     });
 
     stage.add(this.layer);
+
+    this.canvasWidth = this.layer.getWidth() as number;
+    this.canvasHeight = this.layer.getHeight() as number;
+
+    this.clear();
+  }
+
+  private getImageData() {
+    return this.context.getImageData(0, 0, this.canvasWidth, this.canvasHeight);
   }
 
   get isErasing() {
@@ -183,38 +221,95 @@ export class PaintCanvas {
   }
 
   private clear() {
-    this.context?.clearRect(
-      0,
-      0,
-      this.image.getWidth(),
-      this.image.getHeight()
-    );
+    let imageData = this.getImageData();
 
+    for (let i = 0; i < this.canvasWidth * this.canvasHeight; ++i) {
+      let index = i * 4;
+      imageData.data[index] = 255;
+      imageData.data[index + 1] = 255;
+      imageData.data[index + 2] = 255;
+      imageData.data[index + 3] = 255;
+    }
+
+    this.context.putImageData(imageData, 0, 0);
     this.layer.batchDraw();
   }
 
+
+  private hexToRGB(hex: string) {
+    return [
+      parseInt(hex.substring(1, 3), 16),
+      parseInt(hex.substring(3, 5), 16),
+      parseInt(hex.substring(5, 7), 16),
+      255
+    ];
+  }
+
+  private drawPixel(point: [number, number], color: string, image: ImageData) {
+    let colorRGB = this.hexToRGB(color);
+    let width = this.layer.getWidth() as number;
+    let height = this.layer.getHeight() as number;
+
+    let y = Math.floor(point[1]);
+    let x = Math.floor(point[0]);
+
+    if (x < 0 || x >= width) return;
+    if (y < 0 || y >= height) return;
+
+    let index = (y * width + x) * 4;
+
+    image.data[index] = colorRGB[0];
+    image.data[index + 1] = colorRGB[1];
+    image.data[index + 2] = colorRGB[2];
+    image.data[index + 3] = 255;
+  }
+
+  /*
+    Based on: https://stackoverflow.com/questions/1201200/fast-algorithm-for-drawing-filled-circles
+    (AlexGeorg)
+  */
+  private drawCircle(point: [number, number], radius: number, color: string, image: ImageData) {
+    for (let x = -radius; x < radius; ++x) {
+      let hh = Math.sqrt(radius * radius - x * x);
+
+      let rx = point[0] + x;
+      let ph = point[1] + hh;
+
+      for (let y = point[1] - hh; y <= ph; ++y) {
+        this.drawPixel([rx, y], color, image);
+      }
+    }
+  }
+
+  private drawSegment(points: [number, number][], radius: number, color: string, image: ImageData) {
+    if (points.length < this.requiredPoints) return;
+
+    const interp = new CurveInterpolator(
+      points,
+      { tension: 0, alpha: 0.5 }
+    );
+
+    const segments = 100;
+
+    const interpolated_points = interp.getPoints(segments);
+
+    for (let point of interpolated_points) {
+      this.drawCircle(point, radius, color, image);
+    }
+  }
+
   private drawPoint(
-    pointA: Konva.Vector2d,
-    pointB: Konva.Vector2d,
+    points: [number, number][],
     brush: Brush,
-    isErasing: boolean
+    isErasing: boolean,
+    image: ImageData
   ) {
-    if (this.context == null) {
+    if (points.length == 0) {
+      console.warn("Trying to draw point with 0 points?");
       return;
     }
 
-    let context = this.context as CanvasRenderingContext2D;
-
-    if (isErasing) {
-      context.globalCompositeOperation = "destination-out";
-    } else {
-      context.globalCompositeOperation = "source-over";
-    }
-
-    context.lineWidth = brush.strokeWidth;
-    context.strokeStyle = brush.color;
-    context.fillStyle = brush.color;
-    context.lineJoin = "round";
+    let color = isErasing ? "#ffffff" : brush.color;
 
     let radius = brush.strokeWidth;
 
@@ -226,52 +321,47 @@ export class PaintCanvas {
       radius /= 2;
     }
 
-    // Did the player click (drawing a dot),
-    // or did they draw an actual line?
-    let isDot: boolean = pointA == pointB;
+    this.drawCircle(points[0], radius, color, image);
 
-    context.beginPath();
-
-    if (isDot) {
-      this.context?.arc(
-        pointA.x,
-        pointB.y,
+    if (points.length > 1) {
+      this.drawSegment(
+        points,
         radius,
-        0,
-        Math.PI * 2
+        color,
+        image
       );
-    } else {
-      this.context?.moveTo(pointA.x, pointA.y);
-      this.context?.lineTo(pointB.x, pointB.y);
     }
-
-    context.closePath();
-
-    if (isDot)
-      context.fill();
-    else
-      context.stroke();
-
-    this.layer.batchDraw();
   }
 
-  private recreateBuffer(buffer: PaintAction[]) {
+  private recreateBuffer(buffer: PaintAction[], image: ImageData) {
     for (let action of buffer) {
       switch (action.type) {
         case PaintActionType.Draw: {
           for (let point of action.points) {
-            this.drawPoint(point[0], point[1], action.brush, false);
+            this.drawPoint(point, action.brush, false, image);
           }
           break;
         }
         case PaintActionType.Erase: {
           for (let point of action.points) {
-            this.drawPoint(point[0], point[1], action.brush, true);
+            this.drawPoint(point, action.brush, true, image);
           }
+          break;
+        }
+        case PaintActionType.Fill: {
+          let point = action.points[0][0];
+          this.fill(
+            point[0],
+            point[1],
+            action.brush.color,
+            image
+          );
           break;
         }
       }
     }
+
+    this.hasReverted = true;
   }
 
   // Recreates the art canvas by going through undo buffer
@@ -286,7 +376,12 @@ export class PaintCanvas {
     let lastAction = this.undoBuffer.pop() as PaintAction;
     this.redoBuffer.push(lastAction);
 
-    this.recreateBuffer(this.undoBuffer);
+    let imageData = this.getImageData();
+
+    this.recreateBuffer(this.undoBuffer, imageData);
+
+    this.context.putImageData(imageData, 0, 0);
+    this.layer.batchDraw();
   }
 
   redo() {
@@ -298,114 +393,152 @@ export class PaintCanvas {
     let lastAction = this.redoBuffer.pop() as PaintAction;
     this.undoBuffer.push(lastAction);
 
-    this.recreateBuffer(this.undoBuffer);
+    let imageData = this.getImageData();
+
+    this.recreateBuffer(this.undoBuffer, imageData);
+
+    this.context.putImageData(imageData, 0, 0);
+    this.layer.batchDraw();
   }
 
-  fill(x: number, y: number, newColor: string) {
+  fill(x: number, y: number, newColor: string, image?: ImageData) {
     let width = this.layer.getWidth() as number;
     let height = this.layer.getHeight() as number;
 
-    let imageData = this.context?.getImageData(0, 0, width, height) as ImageData;
-
-    const isSameColor = (a: number[], b: number[], tolerance: number) => {
-      return Math.abs(a[0] - b[0]) <= tolerance &&
-        Math.abs(a[1] - b[1]) <= tolerance &&
-        Math.abs(a[2] - b[2]) <= tolerance &&
-        Math.abs(a[3] - b[3]) <= tolerance;
+    const isValidCoord = (x: number, y: number) => {
+      return x >= 0 && x < width &&
+        y >= 0 && y < height;
     };
 
-    const hexToRGB = (hex: string) =>
-      [
-        parseInt(hex.substring(1, 3), 16),
-        parseInt(hex.substring(3, 5), 16),
-        parseInt(hex.substring(5, 7), 16),
-        255
-      ];
-
-    const newColorRGB = hexToRGB(newColor);
-
-    let pickedIndex = (y * width + x) * 4;
-    const oldColorRGB = [
-      imageData.data[pickedIndex],
-      imageData.data[pickedIndex + 1],
-      imageData.data[pickedIndex + 2],
-      imageData.data[pickedIndex + 3],
-    ];
-    if (isSameColor(newColorRGB, oldColorRGB, 0)) {
+    if (!isValidCoord(x, y)) {
+      console.warn(`Passing in invalid coordinates: ${x}${y}`);
       return;
     }
 
-    console.log(oldColorRGB);
+    let imageData = image ?? this.getImageData();
+    this.context.imageSmoothingEnabled = false;
 
-    let queue: Array<[number, number]> = [];
-    let visited: Set<string> = new Set();
+    const isSameColor = (a: number[], b: number[], tolerance: number) => {
+      const dr = Math.abs(a[0] - b[0]);
+      const dg = Math.abs(a[1] - b[1]);
+      const db = Math.abs(a[2] - b[2]);
+      let res = (dr * dr + dg * dg + db * db) <= tolerance * tolerance;
+      return res;
+    };
 
-    let currentHead = 0;
-
-    queue.push([x, y]);
-
-    while (currentHead < queue.length) {
-      let currentPixelCoord = queue[currentHead++];
-      visited.add(`${currentPixelCoord[0]},${currentPixelCoord[1]}`);
-      let i = (currentPixelCoord[1] * width + currentPixelCoord[0]) * 4;
-
-      if (i < 0 || i >= width * height * 4) {
-        continue;
-      }
-
-      let currentColor = [
+    const getPixelColor = (x: number, y: number) => {
+      let i = (y * width + x) * 4;
+      return [
         imageData.data[i],
         imageData.data[i + 1],
         imageData.data[i + 2],
         imageData.data[i + 3],
       ];
+    };
 
+    const newColorRGB = this.hexToRGB(newColor);
 
-      if (!isSameColor(oldColorRGB, currentColor, 50)) {
-        // console.log(oldColorRGB, currentColor);
-        continue;
+    let oldColorRGB = getPixelColor(x, y);
+    if (isSameColor(newColorRGB, oldColorRGB, 0)) {
+      return;
+    }
+
+    const getSpan = (x: number, y: number) => {
+      let start = 0;
+      let end = width - 1;
+      for (let startX = x; startX < width; ++startX) {
+        const currentColorRGB = getPixelColor(startX, y);
+        if (!isSameColor(currentColorRGB, oldColorRGB, 0)) {
+          end = startX - 1
+          break;
+        }
+      }
+      for (let startX = x; startX >= 0; --startX) {
+        const currentColorRGB = getPixelColor(startX, y);
+        if (!isSameColor(currentColorRGB, oldColorRGB, 0)) {
+          start = startX + 1;
+          break;
+        }
+      }
+      return [start, end, y];
+    }
+
+    let pixel_span: Array<[number, number]> = [];
+    pixel_span.push([Math.floor(x), Math.floor(y)]);
+
+    const checkPixelForSpan = (x: number, y: number, isInsideSpan: boolean) => {
+      if (!isValidCoord(x, y)) {
+        return [false, x];
       }
 
-      imageData.data[i] = newColorRGB[0];
-      imageData.data[i + 1] = newColorRGB[1];
-      imageData.data[i + 2] = newColorRGB[2];
-      imageData.data[i + 3] = 255;
-
-      const getColor = (x: number, y: number) => {
-        let i = (y * width + x) * 4;
-
-        return [
-          imageData.data[i],
-          imageData.data[i + 1],
-          imageData.data[i + 2],
-          imageData.data[i + 3],
-        ];
+      let topPixelColor = getPixelColor(x, y);
+      if (
+        isSameColor(topPixelColor, oldColorRGB, 0) &&
+        !isInsideSpan
+      ) {
+        let adjacentSpan = getSpan(x, y);
+        pixel_span.push([x, y]);
+        return [true, adjacentSpan[1]];
       }
 
-      const isValidCoord = (x: number, y: number) => {
-        return x >= 0 &&
-          x < width &&
-          y >= 0 &&
-          y < height &&
-          !visited.has(`${x},${y}`) &&
-          !isSameColor(getColor(x, y), newColorRGB, 50);
-      };
+      return [false, x];
+    };
 
-      if (isValidCoord(currentPixelCoord[0] + 1, currentPixelCoord[1])) {
-        queue.push([currentPixelCoord[0] + 1, currentPixelCoord[1]]);
+    while (pixel_span.length > 0) {
+      let currentPixelCoord = pixel_span.pop() as [number, number];
+      let currentSpan = getSpan(currentPixelCoord[0], currentPixelCoord[1]);
+
+      // Used for checking adjacent rows to current row filled
+      let isInsideSpan = false;
+
+      let currentY = currentSpan[2];
+
+      // Check above
+
+      for (let currentX = currentSpan[0]; currentX <= currentSpan[1]; ++currentX) {
+        let state = checkPixelForSpan(currentX, currentY - 1, isInsideSpan);
+        isInsideSpan = state[0] as boolean;
+        let newX = state[1] as number;
+        if (newX < currentX) continue;
+        currentX = newX;
       }
-      if (isValidCoord(currentPixelCoord[0] - 1, currentPixelCoord[1])) {
-        queue.push([currentPixelCoord[0] - 1, currentPixelCoord[1]]);
+
+      isInsideSpan = false;
+
+      // Check below
+
+      for (let currentX = currentSpan[0]; currentX <= currentSpan[1]; ++currentX) {
+        let state = checkPixelForSpan(currentX, currentY + 1, isInsideSpan);
+        isInsideSpan = state[0] as boolean;
+        let newX = state[1] as number;
+        if (newX < currentX) continue;
+        currentX = newX;
       }
-      if (isValidCoord(currentPixelCoord[0], currentPixelCoord[1] + 1)) {
-        queue.push([currentPixelCoord[0], currentPixelCoord[1] + 1]);
-      }
-      if (isValidCoord(currentPixelCoord[0], currentPixelCoord[1] - 1)) {
-        queue.push([currentPixelCoord[0], currentPixelCoord[1] - 1]);
+
+      // Fill in current span
+
+      for (let currentX = currentSpan[0]; currentX <= currentSpan[1]; ++currentX) {
+        let i = (currentY * width + currentX) * 4;
+
+        imageData.data[i] = newColorRGB[0];
+        imageData.data[i + 1] = newColorRGB[1];
+        imageData.data[i + 2] = newColorRGB[2];
+        imageData.data[i + 3] = 255;
       }
     }
 
-    this.context?.putImageData(imageData, 0, 0);
+    if (image) return;
+
+    this.context.putImageData(imageData, 0, 0);
     this.layer.batchDraw();
+    this.hasFilled = true;
+
+    let fillAction: PaintAction = {
+      points: [[[x, y]]],
+      type: PaintActionType.Fill,
+      brush: { ...this.currentBrush }
+    };
+
+    this.undoBuffer.push(fillAction);
   }
 }
