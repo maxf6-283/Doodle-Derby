c1a5730bba45143962c66cd63e64a0a5044348ee
AllocatedArtist <59222318+AllocatedArtist@users.noreply.github.com>
2026-02-12 23:46:32 -0800

    netcode hell begins

--- gameplay_page.tsx
+++ gameplay_page.tsx
@@ -1,9 +1,155 @@
 import { Page } from "./page";
 import { render } from "solid-js/web"
+import { createSignal, onMount } from "solid-js";
+
+import { getParticipants, PlayerState, me, isHost, RPC } from "playroomkit";
+
+import konva from "konva";
+import { PaintCanvas } from "./api/draw/painting"
+
+// Functions here are throwaways and only serve as substitutes
+
+function pickRandomArtists() {
+  const participants: PlayerState[] = Object.values(getParticipants());
+  let size = participants.length;
+
+  const randInt = () => {
+    return Math.floor(Math.random() * size);
+  }
+
+  let firstIndex = randInt();
+  let secondIndex = 0;
+  do {
+    secondIndex = randInt();
+  } while (secondIndex == firstIndex);
+
+  console.log(`first: ${firstIndex}, second: ${secondIndex}`);
+
+  if (firstIndex < 0 || firstIndex >= size ||
+    secondIndex < 0 || secondIndex >= size ||
+    firstIndex == secondIndex) {
+    alert("bruh picking random artists went wrong...");
+  }
+
+  participants.forEach((player: PlayerState) => player.setState("isArtist", false));
+
+  participants[firstIndex].setState("isArtist", true);
+  participants[secondIndex].setState("isArtist", true);
+
+}
+
+// This is very hacky! Let's change this as soon as possible!!!
+
+function drawTools(stage: konva.Stage, canvas: HTMLCanvasElement | null) {
+  if (canvas == null) return;
+
+  canvas.width = stage.width();
+  canvas.height = stage.height();
+
+  const small_button = document.getElementById('small-button') as HTMLInputElement;
+  const med_button = document.getElementById('med-button') as HTMLInputElement;
+  const large_button = document.getElementById('large-button') as HTMLInputElement;
+  const color_button = document.getElementById('color-button') as HTMLInputElement;
+
+  let paint = new PaintCanvas(
+    canvas,
+    { x: 0, y: 0 },
+    stage,
+    { color: "#000000", strokeWidth: 5 }
+  );
+
+  function changeBrushSize(size: number) {
+    paint.setBrushStrokeWidth(size);
+  }
+
+  function changeColor(color: string) {
+    console.log("In change colour ", color);
+    paint.setBrushColor(color);
+  }
+
+  small_button.addEventListener('click', () => changeBrushSize(5));
+  med_button.addEventListener('click', () => changeBrushSize(20));
+  large_button.addEventListener('click', () => changeBrushSize(30));
+  color_button.addEventListener('input', () => changeColor(color_button.value));
+
+  window.addEventListener("keydown", ev => {
+    if (ev.key == "e") {
+      paint.setErasing(!paint.isErasing);
+    }
+
+    if (ev.key == "u") {
+      paint.undo();
+    }
+
+    if (ev.key == "r") {
+      paint.redo();
+    }
+
+    if (ev.key == "f") {
+      let x = stage.pointerPos?.x as number;
+      let y = stage.pointerPos?.y as number;
+      paint.fill(Math.floor(x), Math.floor(y), paint.brushColor);
+    }
+  });
+}
+
+const DrawPage = () => {
+  let container: HTMLDivElement | undefined;
+  let canvas: HTMLCanvasElement | undefined;
+
+  onMount(() => {
+    let stage = new konva.Stage({
+      container,
+      width: 600,
+      height: 600
+    });
+
+    drawTools(stage, canvas || null);
+  });
+
+  return (
+    <div ref={container} id='container'>
+      <canvas ref={canvas} />
+    </div>
+  );
+}
+
+const SpectatorPage = () => {
+  let [text, setText] = createSignal("");
+  return (
+    <>
+      <h1>{text()}</h1>
+      <input type="text" onChange={(c) => setText(text => text = c.currentTarget.value)} />
+    </>
+  );
+}
+
+function actualRender(root: HTMLElement) {
+  const currentPlayer = me();
+
+  const Dummy = () => {
+    let isArtist: boolean = currentPlayer.getState("isArtist") ?? false;
+    if (isArtist) {
+      return (<DrawPage />);
+    }
+    return <SpectatorPage />
+  };
+
+  render(() => (<Dummy />), root);
+}
+
+//
 
 export const GameplayPage: Page = {
+
   render(root: HTMLElement) {
+    RPC.register("actualRender", async () => actualRender(root));
+
+    if (isHost()) {
+      pickRandomArtists();
+      RPC.call("actualRender", {}, RPC.Mode.ALL);
+    }
+
     root.innerHTML = "";
-    render(() => (<h1>Hello World!</h1>), root);
   }
 }
