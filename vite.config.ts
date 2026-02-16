35a64ef2ac4128745ece2eb48f78b58c865632c3
AllocatedArtist <59222318+AllocatedArtist@users.noreply.github.com>
2026-02-16 15:49:44 -0800

    cleaned up config

--- vite.config.ts
+++ vite.config.ts
@@ -7,8 +7,8 @@ export default defineConfig({
   build: {
     rollupOptions: {
       input: {
-        main: "index.html",
         game: "game.html",
+        main: "index.html"
       },
     },
   },
