880f52ef6054f5aa1b389b8e3c3b03285957d6e1
AllocatedArtist <59222318+AllocatedArtist@users.noreply.github.com>
2026-02-16 15:48:57 -0800

    landing page uses solid

---
+++ landing_page.tsx
@@ -0,0 +1,56 @@
+import { render } from "solid-js/web"
+import { Page } from "./page"
+import { createSignal } from "solid-js";
+import { routerNavigate } from "./tiny_router";
+
+function joinLobby(code: string) {
+  if (code.length == 4) {
+    routerNavigate("/lobby");
+    window.location.href = "/#r=R" + code
+  } else {
+    alert("Invalid code");
+  }
+}
+
+function LandingMain() {
+  const [lobbyCode, setLobbyCode] = createSignal("");
+
+  return (
+    <div>
+      <h1>Doodle derby</h1>
+      <button
+        style="background-color: blue; color: white;"
+        onClick={_ => routerNavigate("/lobby")}
+      >
+        CREATE NEW LOBBY
+      </button>
+      <br></br>
+      <button
+        style="background-color: blue; 
+            color: white;"
+        id="join-button"
+        onClick={_ => joinLobby(lobbyCode())}
+      >
+        JOIN LOBBY
+      </button>
+      <input
+        onKeyDown={
+          ev => {
+            if (ev.key === "Enter")
+              joinLobby(lobbyCode())
+          }
+        }
+        type="text"
+        placeholder="CODE"
+        id="join-code"
+        onInput={(element) => setLobbyCode(element.currentTarget.value)}
+      />
+    </div>
+  );
+}
+
+export const LandingPage: Page = {
+  async render(root: HTMLElement) {
+    this.onEnd = render(() => <LandingMain />, root);
+  }
+}
