c683a8707883ab848accaa52be748bb65736da32
maxf6-283 <maxf6.283@gmail.com>
2026-02-22 16:22:52 -0800

    Add podium gifs

--- src/pages/podium_page.tsx
+++ src/pages/podium_page.tsx
@@ -3,19 +3,19 @@ import { getParticipants, myPlayer, getState, PlayerState } from "playroomkit";
 import { Page } from "../../api/page";
 import { routerNavigate } from "../../api/tiny_router";
 import { render } from "solid-js/web";
-import { createSignal, onMount, Show, For } from "solid-js";
+import { createSignal, onMount, Show, For, Setter, Accessor } from "solid-js";
 
 import "../../style/podium-page.css";
 
 function PlayerCard({ player }: { player: PlayerState }) {
-  const name = player.getState("name") || "Guest";
+  const name = player.getState("name") ?? "Guest";
   const characterImg = player.getState("character");
   const accessories: string[] = [
     player.getState("acc_0"),
     player.getState("acc_1"),
     player.getState("acc_2"),
   ].filter((a) => a);
-  const score = player.getState("score");
+  const score = player.getState("score") ?? 0;
 
   return (
     <div class="player-score-card">
@@ -46,21 +46,41 @@ function PlayerCard({ player }: { player: PlayerState }) {
   );
 }
 
-function Podium({ src, player }: { src: string; player: PlayerState | null }) {
+function Podium({
+  src,
+  player,
+  width,
+}: {
+  src: Accessor<string | null>;
+  player: PlayerState | null;
+  width: number;
+}) {
+  console.log("Rerendered");
   return (
     <Show when={player != null}>
-      <div class="podium">
-        <PlayerCard player={player as PlayerState} />
-        <img src={src}></img>
+      <div class="podium" style={{ width: `${width}px` }}>
+        {/* <PlayerCard player={player as PlayerState} /> */}
+        <img src={src() as string}></img>
       </div>
     </Show>
   );
 }
 
+const images = import.meta.glob("/public/podium/*.png", {
+  eager: true,
+  import: "default", // get the final URL
+});
+
+const imageUrls = Object.values(images) as string[];
+
 function PodiumPageMain() {
   const [firstPlace, setFirstPlace] = createSignal<PlayerState | null>(null);
   const [secondPlace, setSecondPlace] = createSignal<PlayerState | null>(null);
   const [thirdPlace, setThirdPlace] = createSignal<PlayerState | null>(null);
+
+  const [firstImage, setFirstImage] = createSignal<string | null>(null);
+  const [secondImage, setSecondImage] = createSignal<string | null>(null);
+  const [thirdImage, setThirdImage] = createSignal<string | null>(null);
 
   onMount(() => {
     const players = Object.values(getParticipants());
@@ -70,7 +90,7 @@ function PodiumPageMain() {
     let thirdPlaceScore = -1;
 
     for (let player of players) {
-      let score = player.getState("score");
+      let score = player.getState("score") ?? 0;
       if (score > firstPlaceScore) {
         firstPlaceScore = score;
         setFirstPlace(player);
@@ -82,13 +102,40 @@ function PodiumPageMain() {
         setThirdPlace(player);
       }
     }
+
+    imageUrls.forEach((src) => {
+      const img = new Image();
+      img.src = src;
+    });
+
+    const next_img = (
+      prefix: string,
+      idx: number,
+      max: number,
+      setter: Setter<string | null>,
+    ) => {
+      setter(`${prefix}${idx}.png`);
+      console.log(`setting ${prefix}${idx}.png`);
+      if (idx < max)
+        setTimeout(() => next_img(prefix, idx + 1, max, setter), 300);
+    };
+
+    setTimeout(() => {
+      next_img("/podium/Third_podium_", 1, 4, setThirdImage);
+    }, 0);
+    setTimeout(() => {
+      next_img("/podium/Second_podium_", 1, 5, setSecondImage);
+    }, 1000);
+    setTimeout(() => {
+      next_img("/podium/First_podium_", 1, 6, setFirstImage);
+    }, 3000);
   });
 
   return (
     <div class="podium-container">
-      <Podium src="/podium/podium-2nd.png" player={secondPlace()} />
-      <Podium src="/podium/podium-1st.png" player={firstPlace()} />
-      <Podium src="/podium/podium-3rd.png" player={thirdPlace()} />
+      <Podium src={secondImage} player={secondPlace()} width={415} />
+      <Podium src={firstImage} player={firstPlace()} width={500} />
+      <Podium src={thirdImage} player={thirdPlace()} width={415} />
     </div>
   );
 }
